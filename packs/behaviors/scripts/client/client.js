97e1285744af70ce560195d53cb6fab1f0ae0dc9
martinKindall <mart256@gmail.com>
2020-05-06 13:49:05 -0400

    Added UIEngine and hello.html interface.

--- packs/behaviors/scripts/client/client.js
+++ packs/behaviors/scripts/client/client.js
@@ -4,6 +4,10 @@ const clientSystem = client.registerSystem(0, 0);
 
 // Setup which events to listen for
 clientSystem.initialize = function () {
+    this.listenForEvent(
+        "minecraft:client_entered_world",
+        (eventData) => this.onClientEnteredWorld(eventData)
+    );
     this.listenForEvent("minecraft:ui_event", (eventData) => this.onUIMessage(eventData));
 };
 
@@ -12,6 +16,14 @@ clientSystem.update = function () {
 	// Any logic that needs to happen every tick on the client.
 };
 
+
+clientSystem.onClientEnteredWorld = function(eventData) {
+    let loadEventData = this.createEventData("minecraft:load_ui");
+    loadEventData.data.path = "hello.html";
+    loadEventData.data.options.is_showing_menu = false;
+    loadEventData.data.options.absorbs_input = true;
+    this.broadcastEvent("minecraft:load_ui", loadEventData);
+};
 
 clientSystem.onUIMessage = function(eventData) {
     Utils.broadcastOnChat(this, `ui event: ${JSON.stringify(eventData)}`);
