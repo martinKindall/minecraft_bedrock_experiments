105e232d01b66c143d0d8711d05ed254c04b7760
martinKindall <mart256@gmail.com>
2020-05-06 20:51:11 -0400

    Creating llama on init.

--- packs/behaviors/scripts/server/server.js
+++ packs/behaviors/scripts/server/server.js
@@ -14,8 +14,19 @@ serverSystem.update = function () {
 
 serverSystem.onStartGame = function(eventData) {
     this.executeCommand("/tp @a 0 8 0", (commandData) => this.commandCallback(commandData) );
+
+    this.createLlama();
 };
 
 serverSystem.commandCallback = function(commandData) {
     Utils.broadcastOnChat(this, `command executed: ${JSON.stringify(commandData)}`);
+};
+
+serverSystem.createLlama = function () {
+    let llama = this.createEntity("entity", "minecraft:llama");
+    let posComponent = this.createComponent(llama, "minecraft:position");
+    posComponent.data.x = -6.5;
+    posComponent.data.y = 5;
+    posComponent.data.z = 0.5;
+    this.applyComponentChanges(llama, posComponent);
 };
